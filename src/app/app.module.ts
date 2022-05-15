d4e60ac22a8ba0001fd44a3a27739af639725a6e
RicielleFreitas <riciellesiqueira881@gmail.com>
2022-05-15 15:01:14 -0300

    criado e adicionado componente de extrato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-tranferencia/nova-transferencia.component';
+import { ExtratoComponent } from './extrato/extrato.component';
 
 @NgModule({
-  declarations: [AppComponent, NovaTransferenciaComponent],
+  declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
 
   imports: [BrowserModule, AppRoutingModule, FormsModule],
 
